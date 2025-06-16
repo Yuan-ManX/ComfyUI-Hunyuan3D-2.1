5669c972ee25da61d508cc5d89f3f6ccdbc4f7fc
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-06-16 14:48:53 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,1 +1,35 @@
+from hy3dpaint.textureGenPipeline import Hunyuan3DPaintPipeline
+from textureGenPipeline import Hunyuan3DPaintPipeline, Hunyuan3DPaintConfig
+from hy3dshape.pipelines import Hunyuan3DDiTFlowMatchingPipeline
 
+
+class LoadHunyuan3DModel:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "model_path": ("STRING", {"default": "tencent/Hunyuan3D-2.1"}),
+                "device": (["cuda", "cpu"], {"default": "cuda"}),
+            }
+        }
+
+    RETURN_TYPES = ("MODEL",)
+    RETURN_NAMES = ("shape_pipeline",)
+    FUNCTION = "load_model"
+    CATEGORY = "Hunyuan3D-2.1"
+
+    def load_model(self, model_path):
+        shape_pipeline = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained('tencent/Hunyuan3D-2.1')
+        
+        return (shape_pipeline,)
+
+
+
+
+
+# let's generate a mesh first
+shape_pipeline = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained('tencent/Hunyuan3D-2.1')
+mesh_untextured = shape_pipeline(image='assets/demo.png')[0]
+
+paint_pipeline = Hunyuan3DPaintPipeline(Hunyuan3DPaintConfig(max_num_view=6, resolution=512))
+mesh_textured = paint_pipeline(mesh_path, image_path='assets/demo.png')
